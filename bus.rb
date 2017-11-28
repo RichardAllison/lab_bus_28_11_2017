84cf8d02b2936bcdf503c233884ac04b8f9fd716
Max Robson <maxwell.robson@gmail.com>
2017-11-28 13:14:02 +0000

    added Person class and spec, added passengers array and tests

--- bus.rb
+++ bus.rb
@@ -1,14 +1,19 @@
 class Bus
 
-  attr_reader :route, :destination
+  attr_reader :route, :destination, :passengers
 
-  def initialize(route, destination)
+  def initialize(route, destination, passengers)
     @route = route
     @destination = destination
+    @passengers = passengers
   end
 
   def drive
     return "Crunch"
   end
-  
+
+  def count_passengers
+    return @passengers.count()
+  end
+
 end
